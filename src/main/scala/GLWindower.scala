ebb1ef43f1b328b462fa7a301591bf57dfa79f32
Dessix <Dessix@Dessix.net>
2017-08-02 02:32:38 -0700

    Added GL binding bits.

--- src/main/scala/GLWindower.scala
+++ src/main/scala/GLWindower.scala
@@ -4,7 +4,11 @@ import util._
 import org.lwjgl
 import org.lwjgl.glfw._
 import org.lwjgl.glfw.GLFW._
-import org.lwjgl.opengl.GL
+import org.lwjgl.opengl
+import opengl.GL33._
+import opengl.GL32._
+import opengl.GL31._
+import opengl.GL30._
 
 import scalaz.Scalaz._
 import scala.concurrent.duration._
@@ -38,6 +42,9 @@ class GLWindower() {
     throw new IllegalStateException("Unable to initialize GLFW")
   }
 
+  glfwDefaultWindowHints()
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
   private val window = GLFWWindowRef.create(glfwCreateWindow(1024, 768, "Silica", 0, 0)) match {
     case Some(h) => h
     case None =>
@@ -51,8 +58,9 @@ class GLWindower() {
         glfwTerminate()
         throw new IllegalStateException("Window handle invalid")
       })
-      if (key === GLFW_KEY_ESCAPE && action === GLFW_PRESS) {
-        win.shouldClose_=(true)
+      (key, action) match {
+        case (GLFW_KEY_ESCAPE, GLFW_PRESS) => win.shouldClose_=(true)
+        case _ => Unit
       }
     }
   }
@@ -61,22 +69,30 @@ class GLWindower() {
 
 
   glfwMakeContextCurrent(window)
-  GL.createCapabilities()
+  lwjgl.opengl.GL.createCapabilities()
 
-  private def processInput(time: Double): Unit = {
+  private def processInput(time: Double, deltaTime: Double): Unit = {
     glfwPollEvents()
   }
 
-  private def render(time: Double): Unit = {
+  private def render(time: Double, deltaTime: Double): Unit = {
     glfwSwapBuffers(window)
+    val vao = glGenVertexArrays()
+    glBindVertexArray(vao)
+    glDeleteVertexArrays(vao)
   }
 
   def runLoop(): Unit = {
     val sleepMillis = 16.6.milliseconds.toMillis
+    var lastTime = -1.0d
     while (!glfwWindowShouldClose(window)) {
       val time = glfwGetTime()
-      processInput(time)
-      render(time)
+      val deltaTime = if (lastTime > 0) { time - lastTime } else { 0 }
+
+      processInput(time, deltaTime)
+      render(time, deltaTime)
+
+      lastTime = time
       Thread.sleep(sleepMillis)
     }
     glfwDestroyWindow(window)
